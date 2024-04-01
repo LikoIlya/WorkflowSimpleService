2bdd1a494464c35c66e1fa05120f5906d8980dee
LikoIlya <LikoIlya@users.noreply.github.com>
2024-04-01 14:04:25 +0000

    ✅ Ready to clone and code.

--- workflowsimpleservice/app.py
+++ workflowsimpleservice/app.py
@@ -23,18 +23,18 @@ def read(*paths, **kwargs):
 
 
 description = """
-project_name API helps you do awesome stuff. 🚀
+workflowsimpleservice API helps you do awesome stuff. 🚀
 """
 
 app = FastAPI(
-    title="project_name",
+    title="workflowsimpleservice",
     description=description,
     version=read("VERSION"),
-    terms_of_service="http://project_name.com/terms/",
+    terms_of_service="http://workflowsimpleservice.com/terms/",
     contact={
-        "name": "author_name",
-        "url": "http://project_name.com/contact/",
-        "email": "author_name@project_name.com",
+        "name": "LikoIlya",
+        "url": "http://workflowsimpleservice.com/contact/",
+        "email": "LikoIlya@workflowsimpleservice.com",
     },
     license_info={
         "name": "The Unlicense",
--- workflowsimpleservice/config.py
+++ workflowsimpleservice/config.py
@@ -5,11 +5,11 @@ from dynaconf import Dynaconf
 HERE = os.path.dirname(os.path.abspath(__file__))
 
 settings = Dynaconf(
-    envvar_prefix="project_name",
+    envvar_prefix="workflowsimpleservice",
     preload=[os.path.join(HERE, "default.toml")],
     settings_files=["settings.toml", ".secrets.toml"],
     environments=["development", "production", "testing"],
-    env_switcher="project_name_env",
+    env_switcher="workflowsimpleservice_env",
     load_dotenv=False,
 )
 
@@ -18,7 +18,7 @@ settings = Dynaconf(
 # How to use this application settings
 
 ```
-from project_name.config import settings
+from workflowsimpleservice.config import settings
 ```
 
 ## Acessing variables
@@ -45,15 +45,15 @@ KEY=value
 
 ### As environment variables
 ```
-export PROJECT_NAME_KEY=value
-export PROJECT_NAME_KEY="@int 42"
-export PROJECT_NAME_KEY="@jinja {{ this.db.uri }}"
-export PROJECT_NAME_DB__uri="@jinja {{ this.db.uri | replace('db', 'data') }}"
+export workflowsimpleservice_KEY=value
+export workflowsimpleservice_KEY="@int 42"
+export workflowsimpleservice_KEY="@jinja {{ this.db.uri }}"
+export workflowsimpleservice_DB__uri="@jinja {{ this.db.uri | replace('db', 'data') }}"
 ```
 
 ### Switching environments
 ```
-PROJECT_NAME_ENV=production project_name run
+workflowsimpleservice_ENV=production workflowsimpleservice run
 ```
 
 Read more on https://dynaconf.com
